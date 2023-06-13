598772c2107cbf25cb894e129d11415553429979
kimseokhee <cat1181123@naver.com>
2023-06-13 17:17:01 +0900

    add: assignment3 ~ 7

--- 과제2/Assignment-3.py
+++ 과제2/Assignment-3.py
@@ -1,15 +1,19 @@
+'''
+03
+6-4.py는 1순위 정확률을 출력한다.
+1순위 정확률과 5순위 정확률을 같이 출력하도록 프로그램을 확장하고, 
+1순위 정확률과 5순위 정확률을 각각 첨부하시오.
+'''
 import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras.datasets import mnist
+from tensorflow.keras.datasets import cifar10
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
-from tensorflow.keras.optimizers import Adam, SGD
+from tensorflow.keras.optimizers import Adam
 
-# MNIST 데이터셋을 읽고 신경망에 입력할 형태로 변환
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-x_train = x_train.reshape(60000, 28, 28, 1)
-x_test = x_test.reshape(10000, 28, 28, 1)
+# CIFAR-10 데이터셋을 읽고 신경망에 입력할 형태로 변환
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
 x_train = x_train.astype(np.float32)/255.0
 x_test = x_test.astype(np.float32)/255.0
 y_train = tf.keras.utils.to_categorical(y_train, 10)
@@ -17,38 +21,45 @@ y_test = tf.keras.utils.to_categorical(y_test, 10)
 
 # 신경망 모델 설계
 cnn = Sequential()
-cnn.add(Conv2D(32, (3, 3), activation='sigmoid', input_shape=(28, 28, 1)))
-cnn.add(Conv2D(64, (3, 3), activation='sigmoid'))
+cnn.add(Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+cnn.add(Conv2D(32, (3, 3), activation='relu'))
+cnn.add(MaxPooling2D(pool_size=(2, 2)))
+cnn.add(Dropout(0.25))
+cnn.add(Conv2D(64, (3, 3), activation='relu'))
+cnn.add(Conv2D(64, (3, 3), activation='relu'))
 cnn.add(MaxPooling2D(pool_size=(2, 2)))
 cnn.add(Dropout(0.25))
 cnn.add(Flatten())
-cnn.add(Dense(128, activation='sigmoid'))
+cnn.add(Dense(512, activation='relu'))
 cnn.add(Dropout(0.5))
 cnn.add(Dense(10, activation='softmax'))
 
 # 신경망 모델 학습
 cnn.compile(loss='categorical_crossentropy',
             optimizer=Adam(), metrics=['accuracy'])
-hist = cnn.fit(x_train, y_train, batch_size=128, epochs=12,
+hist = cnn.fit(x_train, y_train, batch_size=128, epochs=30,
                validation_data=(x_test, y_test), verbose=2)
 
 # 신경망 모델 정확률 평가
 res = cnn.evaluate(x_test, y_test, verbose=0)
 print("1순위 정확률은", res[1]*100)
 
-# Top-5 정확률 평가
-pred = cnn.predict(x_test)
-top_5 = tf.keras.metrics.top_k_categorical_accuracy(y_test, pred, k=5)
-print("5순위 정확률은", np.mean(top_5)*100)
-
+# 5순위 정확률 계산
+y_pred = cnn.predict(x_test)
+sorted_indices = np.argsort(y_pred, axis=1)
+correct = 0
+for i in range(len(y_test)):
+    if np.argmax(y_test[i]) in sorted_indices[i, -5:]:
+        correct += 1
+print('5순위 정확률 ', correct / len(y_test) * 100)
 
 
 # 정확률 그래프
 plt.plot(hist.history['accuracy'])
 plt.plot(hist.history['val_accuracy'])
 plt.title('Model accuracy')
-plt.xlabel('Epoch')
 plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
 plt.legend(['Train', 'Validation'], loc='best')
 plt.grid()
 plt.show()
@@ -57,8 +68,9 @@ plt.show()
 plt.plot(hist.history['loss'])
 plt.plot(hist.history['val_loss'])
 plt.title('Model loss')
-plt.xlabel('Epoch')
 plt.ylabel('Loss')
+plt.xlabel('Epoch')
 plt.legend(['Train', 'Validation'], loc='best')
 plt.grid()
 plt.show()
+cnn.save("my_cnn.h5")
